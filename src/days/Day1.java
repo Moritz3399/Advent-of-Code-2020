6e07cc13c6865fd1463e773b2f48cf79cdce8f9e
Moritz3399 <moritzkordt@web.de>
2020-12-02 23:36:04 +0100

    Small improvements for day 1 and InputReader

--- src/days/Day1.java
+++ src/days/Day1.java
@@ -6,21 +6,21 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Day1 {
-    private int[] expenses;
+    private final int[] expenses;
 
     private Day1(String[] input) {
         this.expenses = Stream.of(input).mapToInt(Integer::parseInt).toArray();
 
         int[] partA = findEntries(2,0, new int[0], -1);
-        System.out.println(String.format("First Answer: %s with %s",Arrays.stream(partA).reduce(1, (a,b) -> a*b),Arrays.toString(partA)));
+        System.out.printf("First Answer: %s with %s%n",Arrays.stream(partA).reduce(1, (a, b) -> a*b),Arrays.toString(partA));
 
         int[] partB = findEntries(3,0, new int[0], -1);
-        System.out.println(String.format("Second Answer: %s with %s",Arrays.stream(partB).reduce(1, (a,b) -> a*b),Arrays.toString(partB)));
+        System.out.printf("Second Answer: %s with %s%n",Arrays.stream(partB).reduce(1, (a, b) -> a*b),Arrays.toString(partB));
     }
 
     private int[] findEntries(int howManyEntriesToFind, int level, int[] previousLevels, int previousIndex) {
         int[] levels = Arrays.copyOf(previousLevels, previousLevels.length + 1);
-        for (int i = 0; i < expenses.length; i++) {
+        for (int i = previousIndex + 1; i < expenses.length; i++) {
             if(i == previousIndex) continue;
             levels[level] = expenses[i];
             if (levels.length != howManyEntriesToFind) {
--- src/util/InputReader.java
+++ src/util/InputReader.java
@@ -5,18 +5,18 @@ import java.util.ArrayList;
 
 public class InputReader {
     public static String[] getLinesOfFile(String path){
-        ArrayList<String> lines = new ArrayList<String>();
+        ArrayList<String> lines = new ArrayList<>();
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(new File((path))));
+            BufferedReader br = new BufferedReader(new FileReader(new File(path)));
             String line;
             while ((line = br.readLine())!= null ){
                 lines.add(line);
             }
-            return lines.toArray(new String[lines.size()]);
+            return lines.toArray(new String[0]);
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("File does not exist: " + path);
         }
         return new String[0];
     }
